d9c347221b063cda8f3eb1e7df0f1cb613bed14d
liquid8796 <hanam.tranle.5@gmail.com>
2025-02-11 16:51:54 +0000

    2025-02-11 23:51:54 +07:00 Asia/Saigon

---
+++ section_10 - resilency/gatewayserver/src/main/java/com/jarvis/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,38 @@
+package com.jarvis.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator jarvisBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/jarvis/accounts/**")
+						.filters( f -> f.rewritePath("/jarvis/accounts/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+							.circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/jarvis/loans/**")
+						.filters( f -> f.rewritePath("/jarvis/loans/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/jarvis/cards/**")
+						.filters( f -> f.rewritePath("/jarvis/cards/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+	}
+}
