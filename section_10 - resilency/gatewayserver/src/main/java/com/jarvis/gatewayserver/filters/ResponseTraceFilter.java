0216ce7f6cb13bedac51cab38c1a9cd75cdfb30b
liquid8796 <hanam.tranle.5@gmail.com>
2025-02-20 23:51:45 +0700

    force commit

--- section_10 - resilency/gatewayserver/src/main/java/com/jarvis/gatewayserver/filters/ResponseTraceFilter.java
+++ section_10 - resilency/gatewayserver/src/main/java/com/jarvis/gatewayserver/filters/ResponseTraceFilter.java
@@ -23,8 +23,11 @@ public class ResponseTraceFilter {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
                 String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+
+                if(!exchange.getRequest().getHeaders().containsKey(FilterUtility.CORRELATION_ID)) {
+                    logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                    exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+                }
             }));
         };
     }
