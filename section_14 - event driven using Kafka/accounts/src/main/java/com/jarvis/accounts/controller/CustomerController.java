b8142c0cf64d158b0d17911b4bf2e74bc3805a48
SACOMBANK\os-namtlh0807962 <os-namtlh0807962@sacombank.com>
2025-04-23 08:41:40 +0000

    2025-04-23 15:41:40 +07:00 Asia/Saigon

---
+++ section_14 - event driven using Kafka/accounts/src/main/java/com/jarvis/accounts/controller/CustomerController.java
@@ -0,0 +1,64 @@
+package com.jarvis.accounts.controller;
+
+import com.jarvis.accounts.dto.CustomerDetailsDto;
+import com.jarvis.accounts.dto.ErrorResponseDto;
+import com.jarvis.accounts.service.ICustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(
+    name = "REST API for Customers in Banking",
+    description = "REST APIs in Banking to FETCH customer details"
+)
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    private ICustomerService customerService;
+
+    public CustomerController(ICustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Operation(
+        summary = "Fetch Customer Details REST API",
+        description = "REST API to fetch Customer details based on a mobile number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        )
+    })
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(
+            @RequestHeader("jarvisbank-correlation-id") String correlationId,
+            @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") String mobileNumber){
+        logger.debug("fetchCustomerDetails method start");
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
+    }
+}
